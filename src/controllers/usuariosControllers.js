4f0b7bb662808c35137535d40d00745a77754acf
MariaLuizaCostaGoes <maria.goes@sptech.school>
2025-06-01 14:43:29 -0300

    Integrando API na pagina de login e cadastro

--- src/controllers/usuariosControllers.js
+++ src/controllers/usuariosControllers.js
@@ -6,57 +6,48 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send('Seu email está undefined!');
     } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
+        res.status(400).send('Sua senha está indefinida!');
     } else {
-
         usuarioModel.autenticar(email, senha)
             .then(
                 function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
+                if (resultadoAutenticar.length == 1) {
+                    console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
+                    res.json({
+                        idUsuario: resultadoAutenticar[0].idUsuario,
+                        email: resultadoAutenticar[0].email,
+                        nome: resultadoAutenticar[0].nome,
+                        senha: resultadoAutenticar[0].senha,
+                    });
+
+                } else if (resultadoAutenticar.length == 0) {
+                    res.status(403).send('Email e/ou senha inválido(s)');
+                } else {
+                    res.status(403).send('Mais de um usuário com o mesmo login e senha!');
                 }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+            }
+        ).catch(function (erro) {
+                console.log(erro);
+                console.log('\nHouve um erro ao realizar o login! Erro: ', erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
     }
-
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
-    // Faça as validações dos valores
+    
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
@@ -66,7 +57,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -84,11 +75,13 @@ function cadastrar(req, res) {
     }
 }
 
+//Quiz
+
 function pegarDescricao(req, res) {
     var idVariacao = req.params.idVariacao
-    usuarioModel.pegarDescricao(idVariacao).then(function(resultado){
+    usuarioModel.pegarDescricao(idVariacao).then(function (resultado) {
         res.status(200).json(resultado)
-    }).catch(function(erro){
+    }).catch(function (erro) {
         res.status(500).json(erro.sqlMessage)
     })
 }
@@ -96,9 +89,9 @@ function pegarDescricao(req, res) {
 function enviarDadosQuiz(req, res) {
     var fkVariacao = req.body.fkVariacaoServer
     var fkUsuario = req.body.fkUsuarioServer
-    usuarioModel.enviarDadosQuiz(fkVariacao, fkUsuario).then(function(resultado){
-        res.status(200).send({fkVariacao:fkVariacao, fkUsuario:fkUsuario})
-    }).catch(function(erro){
+    usuarioModel.enviarDadosQuiz(fkVariacao, fkUsuario).then(function (resultado) {
+        res.status(200).send({ fkVariacao: fkVariacao, fkUsuario: fkUsuario })
+    }).catch(function (erro) {
         res.status(500).json(erro.sqlMessage)
     })
 
